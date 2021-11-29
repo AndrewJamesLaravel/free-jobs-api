01b161e3a9bd1018a9393c6e31865798038daab9
andrew <chibarandrey@gmail.com>
2021-11-29 18:22:30 +0700

    second commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
-require('express-async-errors');
+require('dotenv').config()
+require('express-async-errors')
 // extra security packages
 const helmet = require('helmet')
 const cors = require('cors')
@@ -18,9 +18,8 @@ const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
 
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
-const {login} = require("./controllers/auth");
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 app.set('trust proxy', 1)
 app.use(
@@ -30,30 +29,32 @@ app.use(
     })
 )
 
-app.use(express.json());
+app.use(express.json())
 app.use(helmet())
 app.use(cors())
 app.use(xss())
 
-
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>')
+})
 // routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
-    );
+    )
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
 
-start();
+start()
